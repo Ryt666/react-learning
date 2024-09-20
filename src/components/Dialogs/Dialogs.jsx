01601155f80bbc101e3ec02f8858a990290e7654
palina.patapava <palina.patapava@azati.com>
2024-09-20 17:43:08 +0300

    22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import s from './Dialogs.module.css';
+import { NavLink } from "react-router-dom";
 
 export default function Dialogs() {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+            <NavLink path='/dialogs/1' element={Dialogs}>
+<div className={s.dialog}> Dima</div>
+    </NavLink>
+    <NavLink path='/dialogs/2' element={Dialogs}>
+<div className={s.dialog}> Sasha</div>
+    </NavLink>
+    <NavLink path='/dialogs/3' element={Dialogs}>
+<div className={s.dialog}> Masha</div>
+    </NavLink  >
+    <NavLink path='/dialogs/4' element={Dialogs}>
+<div className={s.dialog}> Kolya</div>
+    </NavLink>
+    <NavLink path='/dialogs/5' element={Dialogs}>
+<div className={s.dialog}> Kirill</div>
+    </NavLink>
+            </div>
+
+            <div className={s.dialog_massages}>
+                <div className={s.massage}>hi</div>
+                <div className={s.massage}>how are you?</div>
+                <div className={s.massage}>okay</div>
+            </div>
         </div>
     )
 }
